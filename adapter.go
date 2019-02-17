8a22ffe9d6947d89f5e1c14555d074c5f7ac2d48
SpacyCoder <sral_h@hotmail.com>
2019-02-17 18:47:26 +0100

    initial commit

---
+++ adapter.go
@@ -0,0 +1,320 @@
+// Copyright 2018 The casbin Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package mongodbadapter
+
+import (
+	"errors"
+	"strconv"
+
+	"github.com/casbin/casbin/model"
+	"github.com/casbin/casbin/persist"
+	"github.com/spacycoder/test/cosmos"
+)
+
+// CasbinRule represents a rule in Casbin.
+type CasbinRule struct {
+	ID    string `json:"id"`
+	PType string `json:"pType"`
+	V0    string `json:"v0"`
+	V1    string `json:"v1"`
+	V2    string `json:"v2"`
+	V3    string `json:"v3"`
+	V4    string `json:"v4"`
+	V5    string `json:"v5"`
+}
+
+// adapter represents the MongoDB adapter for policy storage.
+type adapter struct {
+	collection *cosmos.Collection
+	db         *cosmos.Database
+	filtered   bool
+}
+
+// NewAdapter is the constructor for Adapter. If database name is not provided
+func NewAdapter(url, database string) persist.Adapter {
+	client := cosmos.New(url)
+	db := client.Database(database)
+	collection := db.Collection("casbinRules")
+	a := &adapter{collection: collection, db: db}
+	a.filtered = false
+	return a
+}
+
+// NewFilteredAdapter is the constructor for FilteredAdapter.
+// Casbin will not automatically call LoadPolicy() for a filtered adapter.
+func NewFilteredAdapter(url, database string) persist.FilteredAdapter {
+	a := NewAdapter(url, database).(*adapter)
+	a.filtered = true
+
+	return a
+}
+
+// @TODO implement
+func (a *adapter) dropTable() error {
+	/* 	_, err := a.collection.Delete()
+	   	if err != nil {
+	   		return err
+	   	}
+	   	_, err = a.db.Collections().Create(cosmos.CollectionDefinition{Resource: cosmos.Resource{ID: "casbinRules"}, PartitionKey: cosmos.PartitionKeyDefinition{Paths: []string{"/pType"}, Kind: "Hash"}})
+	*/
+
+	lines := []CasbinRule{}
+	_, err := a.collection.Documents().ReadAll(&lines, cosmos.CrossPartition())
+	if err != nil {
+		return err
+	}
+
+	for _, line := range lines {
+		_, err := a.collection.Document(line.ID).Delete(cosmos.PartitionKey(line.PType))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func loadPolicyLine(line CasbinRule, model model.Model) {
+	key := line.PType
+	sec := key[:1]
+
+	tokens := []string{}
+	if line.V0 != "" {
+		tokens = append(tokens, line.V0)
+	} else {
+		goto LineEnd
+	}
+
+	if line.V1 != "" {
+		tokens = append(tokens, line.V1)
+	} else {
+		goto LineEnd
+	}
+
+	if line.V2 != "" {
+		tokens = append(tokens, line.V2)
+	} else {
+		goto LineEnd
+	}
+
+	if line.V3 != "" {
+		tokens = append(tokens, line.V3)
+	} else {
+		goto LineEnd
+	}
+
+	if line.V4 != "" {
+		tokens = append(tokens, line.V4)
+	} else {
+		goto LineEnd
+	}
+
+	if line.V5 != "" {
+		tokens = append(tokens, line.V5)
+	} else {
+		goto LineEnd
+	}
+
+LineEnd:
+	model[sec][key].Policy = append(model[sec][key].Policy, tokens)
+}
+
+// LoadPolicy loads policy from database.
+func (a *adapter) LoadPolicy(model model.Model) error {
+	return a.LoadFilteredPolicy(model, nil)
+}
+
+// LoadFilteredPolicy loads matching policy lines from database. If not nil,
+// the filter must be a valid MongoDB selector.
+func (a *adapter) LoadFilteredPolicy(model model.Model, filter interface{}) error {
+	lines := []CasbinRule{}
+	if filter == nil {
+		a.filtered = false
+		_, err := a.collection.Documents().ReadAll(&lines, cosmos.CrossPartition())
+		if err != nil {
+			return err
+		}
+	} else {
+		a.filtered = true
+	}
+
+	for _, line := range lines {
+		loadPolicyLine(line, model)
+	}
+
+	return nil
+
+}
+
+// IsFiltered returns true if the loaded policy has been filtered.
+func (a *adapter) IsFiltered() bool {
+	return a.filtered
+}
+
+func savePolicyLine(ptype string, rule []string) CasbinRule {
+	line := CasbinRule{
+		PType: ptype,
+	}
+
+	if len(rule) > 0 {
+		line.V0 = rule[0]
+	}
+	if len(rule) > 1 {
+		line.V1 = rule[1]
+	}
+	if len(rule) > 2 {
+		line.V2 = rule[2]
+	}
+	if len(rule) > 3 {
+		line.V3 = rule[3]
+	}
+	if len(rule) > 4 {
+		line.V4 = rule[4]
+	}
+	if len(rule) > 5 {
+		line.V5 = rule[5]
+	}
+
+	return line
+}
+
+// SavePolicy saves policy to database.
+func (a *adapter) SavePolicy(model model.Model) error {
+	if a.filtered {
+		return errors.New("cannot save a filtered policy")
+	}
+	if err := a.dropTable(); err != nil {
+		return err
+	}
+
+	var lines []CasbinRule
+
+	for ptype, ast := range model["p"] {
+		for _, rule := range ast.Policy {
+			line := savePolicyLine(ptype, rule)
+			lines = append(lines, line)
+		}
+	}
+
+	for ptype, ast := range model["g"] {
+		for _, rule := range ast.Policy {
+			line := savePolicyLine(ptype, rule)
+			lines = append(lines, line)
+		}
+	}
+
+	for _, line := range lines {
+		_, err := a.collection.Documents().Create(&line, cosmos.PartitionKey(line.PType))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// AddPolicy adds a policy rule to the storage.
+func (a *adapter) AddPolicy(sec string, ptype string, rule []string) error {
+	line := savePolicyLine(ptype, rule)
+	_, err := a.collection.Documents().Create(&line, cosmos.PartitionKey(line.PType))
+	return err
+}
+
+// RemovePolicy removes a policy rule from the storage.
+// @TODO IMPLEMENT
+func (a *adapter) RemovePolicy(sec string, ptype string, rule []string) error {
+	query := "SELECT * FROM root WHERE root.pType = @pType"
+	parameters := []cosmos.QueryParam{{Name: "@pType", Value: ptype}}
+	for i, value := range rule {
+		iString := strconv.Itoa(i)
+
+		query += " AND root.v" + iString + " = @v" + iString
+		parameters = append(parameters, cosmos.QueryParam{Name: "@v" + iString, Value: value})
+	}
+
+	querySpec := cosmos.SqlQuerySpec{Parameters: parameters, Query: query}
+	var policies []CasbinRule
+	_, err := a.collection.Documents().Query(&querySpec, &policies, cosmos.PartitionKey(ptype))
+	if err != nil {
+		return err
+	}
+
+	for _, policy := range policies {
+		_, err := a.collection.Document(policy.ID).Delete(cosmos.PartitionKey(policy.PType))
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// RemoveFilteredPolicy removes policy rules that match the filter from the storage.
+// @TODO IMPLEMENT
+func (a *adapter) RemoveFilteredPolicy(sec string, ptype string, fieldIndex int, fieldValues ...string) error {
+	selector := make(map[string]interface{})
+
+	if fieldIndex <= 0 && 0 < fieldIndex+len(fieldValues) {
+		if fieldValues[0-fieldIndex] != "" {
+			selector["v0"] = fieldValues[0-fieldIndex]
+		}
+	}
+	if fieldIndex <= 1 && 1 < fieldIndex+len(fieldValues) {
+		if fieldValues[1-fieldIndex] != "" {
+			selector["v1"] = fieldValues[1-fieldIndex]
+		}
+	}
+	if fieldIndex <= 2 && 2 < fieldIndex+len(fieldValues) {
+		if fieldValues[2-fieldIndex] != "" {
+			selector["v2"] = fieldValues[2-fieldIndex]
+		}
+	}
+	if fieldIndex <= 3 && 3 < fieldIndex+len(fieldValues) {
+		if fieldValues[3-fieldIndex] != "" {
+			selector["v3"] = fieldValues[3-fieldIndex]
+		}
+	}
+	if fieldIndex <= 4 && 4 < fieldIndex+len(fieldValues) {
+		if fieldValues[4-fieldIndex] != "" {
+			selector["v4"] = fieldValues[4-fieldIndex]
+		}
+	}
+	if fieldIndex <= 5 && 5 < fieldIndex+len(fieldValues) {
+		if fieldValues[5-fieldIndex] != "" {
+			selector["v5"] = fieldValues[5-fieldIndex]
+		}
+	}
+
+	query := "SELECT * FROM root WHERE root.pType = @pType"
+	parameters := []cosmos.QueryParam{{Name: "@pType", Value: ptype}}
+	for key, value := range selector {
+		query += " AND root." + key + " = @" + key
+		parameters = append(parameters, cosmos.QueryParam{Name: "@" + key, Value: value})
+	}
+
+	querySpec := cosmos.SqlQuerySpec{Parameters: parameters, Query: query}
+	var policies []CasbinRule
+	_, err := a.collection.Documents().Query(&querySpec, &policies, cosmos.PartitionKey(ptype))
+	if err != nil {
+		return err
+	}
+
+	for _, policy := range policies {
+		_, err := a.collection.Document(policy.ID).Delete(cosmos.PartitionKey(policy.PType))
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
---
+++ adapter_test.go
@@ -0,0 +1,230 @@
+// Copyright 2018 The casbin Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package mongodbadapter
+
+import (
+	"os"
+	"testing"
+
+	"github.com/casbin/casbin"
+	"github.com/casbin/casbin/util"
+	"github.com/mongodb/mongo-go-driver/bson"
+)
+
+var testDbURL = os.Getenv("TEST_COSMOS_URL")
+
+func getDbURL() string {
+	return testDbURL
+}
+
+func testGetPolicy(t *testing.T, e *casbin.Enforcer, res [][]string) {
+	t.Helper()
+	myRes := e.GetPolicy()
+	t.Log("Policy: ", myRes)
+
+	if !util.Array2DEquals(res, myRes) {
+		t.Error("Policy: ", myRes, ", supposed to be ", res)
+	}
+}
+
+func initPolicy(t *testing.T) {
+	// Because the DB is empty at first,
+	// so we need to load the policy from the file adapter (.CSV) first.
+	e := casbin.NewEnforcer("examples/rbac_model.conf", "examples/rbac_policy.csv")
+
+	a := NewAdapter(getDbURL(), "casbin")
+	// This is a trick to save the current policy to the DB.
+	// We can't call e.SavePolicy() because the adapter in the enforcer is still the file adapter.
+	// The current policy means the policy in the Casbin enforcer (aka in memory).
+	err := a.SavePolicy(e.GetModel())
+	if err != nil {
+		panic(err)
+	}
+
+	// Clear the current policy.
+	e.ClearPolicy()
+	testGetPolicy(t, e, [][]string{})
+
+	// Load the policy from DB.
+	err = a.LoadPolicy(e.GetModel())
+	if err != nil {
+		panic(err)
+	}
+	testGetPolicy(t, e, [][]string{{"alice", "data1", "read"}, {"bob", "data2", "write"}, {"data2_admin", "data2", "read"}, {"data2_admin", "data2", "write"}})
+}
+
+func TestAdapter(t *testing.T) {
+	initPolicy(t)
+	// Note: you don't need to look at the above code
+	// if you already have a working DB with policy inside.
+
+	// Now the DB has policy, so we can provide a normal use case.
+	// Create an adapter and an enforcer.
+	// NewEnforcer() will load the policy automatically.
+	a := NewAdapter(getDbURL(), "casbin")
+	e := casbin.NewEnforcer("examples/rbac_model.conf", a)
+	testGetPolicy(t, e, [][]string{{"alice", "data1", "read"}, {"bob", "data2", "write"}, {"data2_admin", "data2", "read"}, {"data2_admin", "data2", "write"}})
+
+	// AutoSave is enabled by default.
+	// Now we disable it.
+	e.EnableAutoSave(false)
+
+	// Because AutoSave is disabled, the policy change only affects the policy in Casbin enforcer,
+	// it doesn't affect the policy in the storage.
+	e.AddPolicy("alice", "data1", "write")
+	// Reload the policy from the storage to see the effect.
+	if err := e.LoadPolicy(); err != nil {
+		t.Errorf("Expected LoadPolicy() to be successful; got %v", err)
+	}
+	// This is still the original policy.
+	testGetPolicy(t, e, [][]string{{"alice", "data1", "read"}, {"bob", "data2", "write"}, {"data2_admin", "data2", "read"}, {"data2_admin", "data2", "write"}})
+
+	// Now we enable the AutoSave.
+	e.EnableAutoSave(true)
+
+	// Because AutoSave is enabled, the policy change not only affects the policy in Casbin enforcer,
+	// but also affects the policy in the storage.
+	e.AddPolicy("alice", "data1", "write")
+	// Reload the policy from the storage to see the effect.
+	if err := e.LoadPolicy(); err != nil {
+		t.Errorf("Expected LoadPolicy() to be successful; got %v", err)
+	}
+	// The policy has a new rule: {"alice", "data1", "write"}.
+	testGetPolicy(t, e, [][]string{{"alice", "data1", "read"}, {"bob", "data2", "write"}, {"data2_admin", "data2", "read"}, {"data2_admin", "data2", "write"}, {"alice", "data1", "write"}})
+
+	// Remove the added rule.
+	e.RemovePolicy("alice", "data1", "write")
+	if err := a.RemovePolicy("p", "p", []string{"alice", "data1", "write"}); err != nil {
+		t.Errorf("Expected RemovePolicy() to be successful; got %v", err)
+	}
+
+	if err := e.LoadPolicy(); err != nil {
+		t.Errorf("Expected LoadPolicy() to be successful; got %v", err)
+	}
+	testGetPolicy(t, e, [][]string{{"alice", "data1", "read"}, {"bob", "data2", "write"}, {"data2_admin", "data2", "read"}, {"data2_admin", "data2", "write"}})
+
+	// Remove "data2_admin" related policy rules via a filter.
+	// Two rules: {"data2_admin", "data2", "read"}, {"data2_admin", "data2", "write"} are deleted.
+	e.RemoveFilteredPolicy(0, "data2_admin")
+	if err := e.LoadPolicy(); err != nil {
+		t.Errorf("Expected LoadPolicy() to be successful; got %v", err)
+	}
+
+	testGetPolicy(t, e, [][]string{{"alice", "data1", "read"}, {"bob", "data2", "write"}})
+
+	e.RemoveFilteredPolicy(1, "data1")
+	if err := e.LoadPolicy(); err != nil {
+		t.Errorf("Expected LoadPolicy() to be successful; got %v", err)
+	}
+	testGetPolicy(t, e, [][]string{{"bob", "data2", "write"}})
+
+	e.RemoveFilteredPolicy(2, "write")
+	if err := e.LoadPolicy(); err != nil {
+		t.Errorf("Expected LoadPolicy() to be successful; got %v", err)
+	}
+	testGetPolicy(t, e, [][]string{})
+}
+func TestDeleteFilteredAdapter(t *testing.T) {
+	return
+	a := NewAdapter(getDbURL(), "casbin")
+	e := casbin.NewEnforcer("examples/rbac_tenant_service.conf", a)
+
+	e.AddPolicy("domain1", "alice", "data3", "read", "accept", "service1")
+	e.AddPolicy("domain1", "alice", "data3", "write", "accept", "service2")
+
+	// Reload the policy from the storage to see the effect.
+	if err := e.LoadPolicy(); err != nil {
+		t.Errorf("Expected LoadPolicy() to be successful; got %v", err)
+	}
+	// The policy has a new rule: {"alice", "data1", "write"}.
+	testGetPolicy(t, e, [][]string{{"domain1", "alice", "data3", "read", "accept", "service1"},
+		{"domain1", "alice", "data3", "write", "accept", "service2"}})
+	// test RemoveFiltered Policy with "" fileds
+	e.RemoveFilteredPolicy(0, "domain1", "", "", "read")
+	if err := e.LoadPolicy(); err != nil {
+		t.Errorf("Expected LoadPolicy() to be successful; got %v", err)
+	}
+	testGetPolicy(t, e, [][]string{{"domain1", "alice", "data3", "write", "accept", "service2"}})
+
+	e.RemoveFilteredPolicy(0, "domain1", "", "", "", "", "service2")
+	if err := e.LoadPolicy(); err != nil {
+		t.Errorf("Expected LoadPolicy() to be successful; got %v", err)
+	}
+	testGetPolicy(t, e, [][]string{})
+}
+
+func TestFilteredAdapter(t *testing.T) {
+	return
+	// Now the DB has policy, so we can provide a normal use case.
+	// Create an adapter and an enforcer.
+	// NewEnforcer() will load the policy automatically.
+	a := NewAdapter(getDbURL(), "casbin")
+	e := casbin.NewEnforcer("examples/rbac_model.conf", a)
+
+	// Load filtered policies from the database.
+	e.AddPolicy("alice", "data1", "write")
+	e.AddPolicy("bob", "data2", "write")
+	// Reload the filtered policy from the storage.
+	filter := &bson.M{"v0": "bob"}
+	if err := e.LoadFilteredPolicy(filter); err != nil {
+		t.Errorf("Expected LoadFilteredPolicy() to be successful; got %v", err)
+	}
+	// Only bob's policy should have been loaded
+	testGetPolicy(t, e, [][]string{{"bob", "data2", "write"}})
+
+	// Verify that alice's policy remains intact in the database.
+	filter = &bson.M{"v0": "alice"}
+	if err := e.LoadFilteredPolicy(filter); err != nil {
+		t.Errorf("Expected LoadFilteredPolicy() to be successful; got %v", err)
+	}
+	// Only alice's policy should have been loaded,
+	testGetPolicy(t, e, [][]string{{"alice", "data1", "write"}})
+
+	// Test safe handling of SavePolicy when using filtered policies.
+	if err := e.SavePolicy(); err == nil {
+		t.Errorf("Expected SavePolicy() to fail for a filtered policy")
+	}
+	if err := e.LoadPolicy(); err != nil {
+		t.Errorf("Expected LoadPolicy() to be successful; got %v", err)
+	}
+
+	e.RemoveFilteredPolicy(2, "write")
+	if err := e.LoadPolicy(); err != nil {
+		t.Errorf("Expected LoadPolicy() to be successful; got %v", err)
+	}
+	testGetPolicy(t, e, [][]string{})
+}
+
+func TestNewAdapterWithInvalidURL(t *testing.T) {
+	return
+	defer func() {
+		if r := recover(); r == nil {
+			t.Error("Expected recovery from panic")
+		}
+	}()
+
+	_ = NewAdapter("localhost:40001?foo=1&bar=2", "casbin")
+}
+
+func TestNewAdapterWithUnknownURL(t *testing.T) {
+	return
+	defer func() {
+		if r := recover(); r == nil {
+			t.Error("Expected recovery from panic")
+		}
+	}()
+
+	_ = NewAdapter("fakeserver:27017", "casbin")
+}
